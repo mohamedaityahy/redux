44c38bef61cbb4886ef173cd69d6e8e0571212cc
mohamedaityahy <mohamedaityahy1@gmail.com>
2025-02-06 19:31:43 +0100

    small fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { legacy_createStore } from '@reduxjs/toolkit'
+import { legacy_createStore } from 'redux'
 import reducer from './config/reducer.js'
 
 const store = legacy_createStore(reducer)
